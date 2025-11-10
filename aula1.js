1223dcc792eba5f08c1e769661b49f6c3f26660b
normazinh56 <normazinh56@gmail.com>
2025-11-09 23:03:18 -0300

    Update aula1.js

--- aula1.js
+++ aula1.js
@@ -1,1 +1,5 @@
+document.getElementById("botão").addEventListener("click", function() {
+  //window.location.href = "https://google.com"// // para abrir nessa pagina//
+  window.open("https://chatgpt.com", "_blank")  //para abrir na proxima pagina//
+})
 
